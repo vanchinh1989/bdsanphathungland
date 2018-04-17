38c74936e4e5192e82575ef85bc2788a70dc8901
vanchinh1989 <vanchinh1989@gmail.com>
2018-04-17 20:54:54 +0700

    apply setting contact info to footer

--- wp-content/themes/anphathungland/footer.php
+++ wp-content/themes/anphathungland/footer.php
@@ -23,9 +23,9 @@
                     <h5 class="color-theme bold my-sm-3 my-md-2 text-center text-md-left">An Phát Hưng Land</h5>
                     <div class="contact-info">
                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fa fa-map-marker-alt fa-fw"></i></span> <span class="bold">Địa chỉ:</span> P10.03 Tòa nhà Kim Ánh, 78 Duy Tân, Cầu Giấy, Hà Nội</li>
-                            <li><span class="fa-li"><i class="fa fa-phone-volume fa-fw"></i></span> <span class="bold">Hotline:</span> <a href="tel:0436321668">0436.321.668</a> - <a href="tel:0906227669">0906.227.669</a></li>
-                            <li><span class="fa-li"><i class="fa fa-envelope fa-fw"></i></span> <span class="bold">Email:</span> <a href="mailto:contact@anphathungland.com">contact@anphathungland.com</a></li>
+                            <li><span class="fa-li"><i class="fa fa-map-marker-alt fa-fw"></i></span> <span class="bold">Địa chỉ:</span><?php echo get_option('gi_option_diachi'); ?></li>
+                            <li><span class="fa-li"><i class="fa fa-phone-volume fa-fw"></i></span> <span class="bold">Hotline:</span> <a href="tel:<?php echo get_option('gi_option_hotline'); ?>"><?php echo get_option('gi_option_hotline'); ?></a> - <a href="tel:<?php echo get_option('gi_option_hotline1'); ?>"><?php echo get_option('gi_option_hotline1'); ?></a></li>
+                            <li><span class="fa-li"><i class="fa fa-envelope fa-fw"></i></span> <span class="bold">Email:</span> <a href="mailto:<?php echo get_option('gi_option_email'); ?>"><?php echo get_option('gi_option_email'); ?></a></li>
                         </ul>
                     </div>
                 </div>
