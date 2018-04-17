7d5e70ce8ca2e46a6853bbd94044563d080f3dc6
vanchinh1989 <vanchinh1989@gmail.com>
2018-04-17 20:50:44 +0700

    setting: add option contact info

---
+++ wp-content/plugins/contact-info/contact-info.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Plugin Name: Thông tin liên hệ
+ * Plugin URI: http://gisoft.vn
+ * Description: Thiết lập thông tin liên hệ
+ * Version: 1.0 
+ * Author: ChinhBV
+ * Author URI: http://gisoft.vn
+ * License: GPLv2
+ */
+
+if(!class_exists('Plugin_Contact_Information')) {
+    class Plugin_Contact_Information {
+            function __construct() {
+                    if(!function_exists('add_shortcode')) {
+                            return;
+                    }
+                    add_shortcode( 'hello' , array(&$this, 'hello_func') );
+            }
+
+            function hello_func($atts = array(), $content = null) {
+                    extract(shortcode_atts(array('name' => 'World'), $atts));
+                    return '<div><p>Hello '.$name.'!!!</p></div>';
+            }
+    }
+}
+function gi_load() {
+    global $mfpd;
+    $mfpd = new Plugin_Contact_Information();
+}
+add_action( 'plugins_loaded', 'gi_load' );
+
+?>
+
+
+<?php
+function register_mysettings() {
+        register_setting( 'mfpd-settings-group', 'gi_option_diachi' );
+        register_setting( 'mfpd-settings-group', 'gi_option_hotline' );
+        register_setting( 'mfpd-settings-group', 'gi_option_hotline1' );
+        register_setting( 'mfpd-settings-group', 'gi_option_email' );
+        register_setting( 'mfpd-settings-group', 'gi_option_googlemap' );
+}
+ 
+function gi_create_menu() {
+        add_menu_page('Thông tin liên hệ', 'Thông tin liên hệ', 'administrator', __FILE__, 'gi_settings_page',plugins_url('/images/icon.png', __FILE__), 1);
+        add_action( 'admin_init', 'register_mysettings' );
+}
+add_action('admin_menu', 'gi_create_menu'); 
+ 
+function gi_settings_page() {
+?>
+<div class="wrap">
+<h2>Thiết lập thông tin liên hệ</h2>
+<?php if( isset($_GET['settings-updated']) ) { ?>
+    <div id="message" class="updated">
+        <p><strong><?php _e('Settings saved.') ?></strong></p>
+    </div>
+<?php } ?>
+<form method="post" action="options.php">
+    <?php settings_fields( 'mfpd-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row">Địa chỉ</th>
+            <td><input type="text" name="gi_option_diachi" value="<?php echo get_option('gi_option_diachi'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Hotline</th>
+            <td><input type="text" name="gi_option_hotline" value="<?php echo get_option('gi_option_hotline'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Hotline 1</th>
+            <td><input type="text" name="gi_option_hotline1" value="<?php echo get_option('gi_option_hotline1'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Email</th>
+            <td><input type="text" name="gi_option_email" value="<?php echo get_option('gi_option_email'); ?>" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Google Map</th>
+            <td><textarea type="text" name="gi_option_googlemap"><?php echo get_option('gi_option_googlemap'); ?></textarea></td>
+        </tr>
+    </table>
+    <?php submit_button(); ?>
+</form>
+</div>
+<?php } ?>
--- wp-content/themes/anphathungland/templates/contact.php
+++ wp-content/themes/anphathungland/templates/contact.php
@@ -19,24 +19,24 @@
             <span class="fa-li">
               <i class="fa fa-map-marker-alt fa-fw"></i>
             </span>
-            <span class="bold">Địa chỉ:</span> P10.03 Tòa nhà Kim Ánh, 78 Duy Tân, Cầu Giấy, Hà Nội</li>
+            <span class="bold">Địa chỉ:</span> <?php echo get_option( 'gi_option_diachi' );?></li>
           <li class="my-3">
             <span class="fa-li">
               <i class="fa fa-phone-volume fa-fw"></i>
             </span>
             <span class="bold">Hotline:</span>
-            <a href="tel:0436321668">0436.321.668</a> -
-            <a href="tel:0906227669">0906.227.669</a>
+            <a href="tel:<?php echo get_option( 'gi_option_hotline' );?>"><?php echo get_option( 'gi_option_hotline' );?></a> -
+            <a href="tel:<?php echo get_option( 'gi_option_hotline1' );?>"><?php echo get_option( 'gi_option_hotline1' );?></a>
           </li>
           <li class="my-3">
             <span class="fa-li">
               <i class="fa fa-envelope fa-fw"></i>
             </span>
             <span class="bold">Email:</span>
-            <a href="mailto:contact@anphathungland.com">contact@anphathungland.com</a>
+            <a href="mailto:<?php echo get_option( 'gi_option_email' );?>"><?php echo get_option( 'gi_option_email' );?></a>
           </li>
         </ul>
-        <iframe width="100%" height="410px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+        <?php echo get_option( 'gi_option_googlemap' );?>
       </div>
     </div>
 </div>
