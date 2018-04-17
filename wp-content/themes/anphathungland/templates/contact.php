cb1aa4ad15780f35396959dca83802daa091d701
vanchinh1989 <vanchinh1989@gmail.com>
2018-04-17 20:00:14 +0700

    page: contact update contact form 7

--- wp-content/themes/anphathungland/templates/contact.php
+++ wp-content/themes/anphathungland/templates/contact.php
@@ -10,36 +10,7 @@
     <div class="row mb-3">
       <div class="col-lg-7">
         <h3>Send us a Message</h3>
-        <form name="sentMessage" id="contactForm" novalidate>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Full Name:</label>
-              <input type="text" class="form-control" id="name" required data-validation-required-message="Please enter your name.">
-              <p class="help-block"></p>
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Phone Number:</label>
-              <input type="tel" class="form-control" id="phone" required data-validation-required-message="Please enter your phone number.">
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Email Address:</label>
-              <input type="email" class="form-control" id="email" required data-validation-required-message="Please enter your email address.">
-            </div>
-          </div>
-          <div class="control-group form-group">
-            <div class="controls">
-              <label>Message:</label>
-              <textarea rows="10" cols="100" class="form-control" id="message" required data-validation-required-message="Please enter your message"
-                maxlength="999" style="resize:none"></textarea>
-            </div>
-          </div>
-          <div id="success"></div>
-          <button type="submit" class="btn btn-primary" id="sendMessageButton">Send Message</button>
-        </form>
+        <?php echo do_shortcode('[contact-form-7 id="61" title="Liên hệ"]') ?>
       </div>
       <div class="col-lg-5">
         <h3>Contact Details</h3>
