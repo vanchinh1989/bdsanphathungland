5e41c5264eda99596306a7b177f9d713fbc26104
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2018-04-17 09:16:01 +0700

    init all widget

--- wp-content/themes/anphathungland/inc/init.php
+++ wp-content/themes/anphathungland/inc/init.php
@@ -3,6 +3,9 @@ require_once( 'logo.php' );
 require_once( 'menu.php' );
 // Register Custom Navigation Walker
 require_once( 'class-wp-bootstrap-navwalker.php' );
-//
-require_once( 'widget_news.php' );
+// widget
+require_once( 'widget-featured-news.php' );
+require_once( 'widget-featured-products.php' );
+require_once( 'widget-call-now.php' );
+
 require_once( 'sidebar.php' );
