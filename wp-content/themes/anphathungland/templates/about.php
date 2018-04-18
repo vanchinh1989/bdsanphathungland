7ff90c0b92ee160578f4914d1ede4f37d85598d9
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2018-04-18 08:35:10 +0700

    page: about apply

--- wp-content/themes/anphathungland/templates/about.php
+++ wp-content/themes/anphathungland/templates/about.php
@@ -10,16 +10,15 @@
     <div class="row">
       <div class="col-md-12">
         <img class="w-100" src="<?php echo THEME_URI; ?>/images/banner/banner-1.jpg"/>
-        <h2 class="module-title mt-4 color-theme">Thư ngỏ</h2>
-        <h6 class="mb-2">Kính gửi Quý khách hàng!</h6>
+        <?php while ( have_posts() ) : the_post(); ?> 
+        <h2 class="module-title mt-4 color-theme"><?php the_title(); ?></h2>
         <p class="content">
-          Công ty Cổ phần Đầu tư An Phát Hưng (An Phát Hưng Land) xin gửi tới Quý khách hàng lời chào trân trọng!
-          An Phát Hưng Land là doanh nghiệp hoạt động chính trong lĩnh vực đầu tư kinh doanh dịch vụ bất động sản, quản lý và phân phối các dự án bất động sản. An Phát Hưng Land  được thành lập bởi các sáng lập viên là những nhà đầu tư uy tín trong lĩnh vực bất động sản tại Việt Nam cùng với các cá nhân xuất sắc đã và đang hoạt động lâu năm trong linh vực Bất động sản.
-          Là một doanh nghiệp mới trong lĩnh vực bất động sản, nhưng An Phát Hưng Land  được thừa hưởng rất nhiều bài học và những trải nghiệm quý giá của các nhà đầu tư bất động sản lâu năm và uy tín nhất tại Việt Nam, cũng chính là các sáng lập viên của công ty. Bên cạnh đó, An Phát Hưng Land còn là doanh nghiệp quy tụ được một đội ngũ những con người năng động có trí thức, giàu nhiệt huyết luôn phấn đấu vì mục tiêu “uy tín là hàng đầu”.
-          Trong lĩnh vực hoạt động chính của mình là phân phối các dự án bất động sản, chúng tôi luôn thực hiện phương châm hoạt động cho mỗi thành viên là: TẬN TÂM- TRUNG THỰC- DỊCH VỤ CHUYÊN NGHIỆP- THÍCH ỨNG-HỢP TÁC BỀN VỮNG- CHIA SẺ THÀNH CÔNG.
-          Với những thế mạnh về con người, kinh nghiệm và sự hiểu biết về thị trường An Phát Hưng Land mong muốn sẽ thỏa mãn tất cả các nhu cầu về dịch vụ bất động sản của khách hàng, tối đa các lợi ích cho những nhà đầu tư, tạo nên sự minh bạch, rõ ràng góp phần vào sự phát triển bền vững của thị trường bất động sản Việt Nam nói chung.
+          <?php the_content(); ?>
         </p>
-        <h6 class="mt-2">Trân trọng!</h6>
+        <?php
+        endwhile; //resetting the page loop
+        wp_reset_query(); //resetting the page query
+        ?>
       </div>
     </div>
 </div>
