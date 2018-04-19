402dab719f115fa804d62d97dc0d40b914a71269
ChinhBV <buivanchinh.nd@gmail.com>
2018-04-19 23:16:35 +0700

    404 page remove input search

--- wp-content/themes/anphathungland/404.php
+++ wp-content/themes/anphathungland/404.php
@@ -6,16 +6,6 @@
         <?php
                 _e('<h2>404 NOT FOUND</h2>', 'gi');
                 _e('<p>The article you were looking for was not found, but maybe try looking again!</p>', 'gi');
-
-                get_search_form();
-
-                _e('<h3>Content categories</h3>', 'gi');
-                echo '<div class="404-catlist">';
-                wp_list_categories( array( 'title_li' => '' ) );
-                echo '</div>';
-
-                _e('<h3>Tag Cloud</h3>', 'gi');
-                wp_tag_cloud();
         ?>
                 </div>
                 <div class="col-lg-3">
